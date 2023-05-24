7c4feceb073f515e5803c6de2e5e6eeb7d53b70e
nkosiphendule <ydomagazi0108@gmail.com>
2023-05-24 14:40:53 +0200

    add deploy workflow

--- mycv/vite.config.js
+++ mycv/vite.config.js
@@ -1,15 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/deploying-vite-project-example/',
   plugins: [react()],
-  optimizeDeps: {
-    // include: ['linked-dep'],
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-      // include: [/linked-dep/, /node_modules/],
-    },
-  },
-});
+})
